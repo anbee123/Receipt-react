aa6fbf6966fc9b9d44cfeb9e2c116a82b9feb78c
An Bui <anhipchipchip@gmail.com>
2022-12-03 14:30:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+import './App.css';
+import receiptData from "./receipData"
+import React from 'react';
+
+const App = () => {
+  const [receipt, setReceipt] = useState(receiptData)
+  const receipts = receiptData.map((receipt, index) => {
+  return (
+    <>
+    className={}
+    </>
+    
+    
+  )
+  }
+
+
+
+export default App;
